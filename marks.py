1f64b4ae384a8847321e5ccb6bb83eb36b0aa727
Vishnu036 <106552904+Vishnu036@users.noreply.github.com>
2022-08-20 20:35:17 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,13 @@
+n=int(input("enter marks"))
+if n>=90 and n<100:
+    print("S grade")
+elif n>=80 and n<90:
+    print("A grade")
+elif n>=70 and n<80:
+    print("B grade")
+elif n>=60 and n<70:
+    print("C grade")
+elif n>=50 and n<60:
+    print("D grade")
+else:
+    print("E grade")
